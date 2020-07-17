af40be29f0a8b6d3e109e0846228ae9ec78412d3
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-17 20:29:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-programmer_hash[:grace_hopper][:known_for]
+programmer_hash[:grace_hopper]
 end
 
 def dennis_ritchies_language
