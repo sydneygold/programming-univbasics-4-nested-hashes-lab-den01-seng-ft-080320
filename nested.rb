2794cf5e4b64910c73ccdbf2cfb024082d29b368
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-17 20:59:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,7 +102,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for] = alans_new_info
+programmer_hash[:alan_kay][:known_for] = "lesse"
 return programmer_hash
 end
 
