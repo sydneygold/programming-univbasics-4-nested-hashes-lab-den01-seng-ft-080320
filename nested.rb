8a9f8dd22af698d34312e2148bde16df025db35e
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-17 20:42:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-programmer_hash.dennis_ritchies_language.index[0]
+programmer_hash[:dennis_ritchie][:languages].index[0]
 
 
 end
