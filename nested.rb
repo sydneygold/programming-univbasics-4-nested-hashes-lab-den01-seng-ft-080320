9db05fba2d6218617394778519f024dfb0c1f719
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-17 20:57:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -77,9 +77,9 @@ def adding_matz
     }
 
 programmer_hash[:yukihiro_matsumoto] = {
-	{:known_for => "Ruby"}
+	{:known_for => "Ruby"},
 	{:languages => ["LISP", "C"] }
-
+}
 return programmer_hash
 end
 
